886356028fda37286ca7ba61f001c7c31db74ec8
gonzalm7 <gonzalm7@wwu.edu>
2019-06-18 11:21:41 -0700

    created action creators for each REST operation

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,13 @@
 import streams from "../apis/streams";
-import { SIGN_IN, SIGN_OUT } from "./types";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM
+} from "./types";
 export const signIn = userId => {
   return {
     type: SIGN_IN,
@@ -12,7 +20,29 @@ export const signOut = () => {
     type: SIGN_OUT
   };
 };
-/*asynch action creator*/
+
+/*asynch action creators for REST operations*/
 export const createStream = formValues => async dispatch => {
-  streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+  dispatch({ type: CREATE_STREAM, payload: response.data });
+};
+/* fetch single stream*/
+export const fetchSteams = () => async dispatch => {
+  const response = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: response.data });
+};
+/* fetch all streams*/
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: response.data });
+};
+/* edit a stream*/
+export const editStream = (id, formValues) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: response.data });
+};
+/* delete a stream*/
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/$(id)`);
+  dispatch({ type: DELETE_STREAM, payload: id });
 };
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,9 @@
 // define all types used in actions and reducers
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SIGN_OUT";
+//types for REST operations
+export const CREATE_STREAM = "CREATE_STREAM";
+export const FETCH_STREAMS = "FETCH_STREAMS";
+export const FETCH_STREAM = "FETCH_STREAM";
+export const DELETE_STREAM = "DELETE_STREAM";
+export const EDIT_STREAM = "EDIT_STREAM";
